288b82bf5d96d7d875574da770537e00d6038615
Jensen Omega <omega90ej@gmail.com>
2023-09-06 10:34:23 -0700

    still working on react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 // import './App.css'
 
+import { Route, Routes, } from "react-router-dom";
 import { LandingPage } from "./components/LandingPage"
 import { AuthProvider, useAuth } from "./providers/AuthProvider"
 import { CoffeeProvider } from "./providers/CoffeeProvider"
@@ -11,18 +12,25 @@ type AuthContextType = {
 }
 
 function App() {
-
-  const {isLogin}= useAuth() as AuthContextType;
+//can i next the providers as the parent component of the routes/routers or is it the other way around
+//is there a better way to make it clean?
+  // const {isLogin}= useAuth() as AuthContextType;
   return (
     <AuthProvider>
       <CoffeeProvider>
-          {isLogin? (
+      <Routes>
+        <Route path="/" element ={<LandingPage />}>
+        </Route>
+      </Routes> 
+      
+          {/* {isLogin? (
             null
           ): (<LandingPage />)
-          }
+          } */}
       </CoffeeProvider>
     </AuthProvider>
-  )
+  
+    )
 }
 
 export default App
