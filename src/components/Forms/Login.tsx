790151f1945e62739be3e1adf481c00986c1b979
Jensen Omega <omega90ej@gmail.com>
2023-09-08 19:01:37 -0700

    added more styling in login and signup component

--- src/components/Forms/Login.tsx
+++ src/components/Forms/Login.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 
 export const Login = () => {
@@ -8,24 +9,30 @@ export const Login = () => {
 
    return(
       <form className="flex-col items-center">
-         <div className="text-center mb-4">
-            <h2>Login</h2>
+         <div className="text-center text-[#2E1E17] p-5 rounded-xl text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
+            <h2>Signup</h2>
          </div>
-         <div className="flex flex-col space-y-4">
+         <div className="flex flex-col space-y-4 ">
             <div className="flex flex-row">
-               <label htmlFor="" className="mb-2 p-3 ">Username:</label>
-               <input type="text"  className="border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
+               <label htmlFor="" className=" w-32 text-lg mb-2 p-3 ">Username:</label>
+               <input type="text"  className="items-center h-14 w-full max-w-md border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
                onChange={(e) => setUsernameInput(e.target.value)}
                value={usernameInput}
                />
             </div>
             <div className="flex flex-row">
-               <label htmlFor="" className="mb-2 p-3">Password:</label>
-               <input type="text"  className="border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
+               <label htmlFor="" className="w-32 text-lg mb-2 p-3">Password:</label>
+               <input type="text"  className="items-center h-14 w-full max-w-md border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
                onChange={(e) => setPassword(e.target.value)}
                value={password}
                />
             </div>
+            <div className="flex flex-row gap-10">
+               <Link></Link>
+               <input type="submit"  className="items-center h-14 w-full max-w-md border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
+               />
+            </div>
+            {/* add back button */}
          </div>
       </form>
    )
--- src/components/Forms/Signup.tsx
+++ src/components/Forms/Signup.tsx
@@ -11,31 +11,32 @@ export const Signup = () => {
 
    return(
       <form className="flex-col items-center">
-         <div className="text-center mb-4">
-            <h2>Login</h2>
+         <div className="text-center text-[#2E1E17] p-5 rounded-xl text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
+            <h2>Signup</h2>
          </div>
-         <div className="flex flex-col space-y-4">
+         <div className="flex flex-col space-y-4 ">
             <div className="flex flex-row">
-               <label htmlFor="" className="mb-2 p-3 ">Username:</label>
-               <input type="text"  className="border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
+               <label htmlFor="" className=" w-32 text-lg mb-2 p-3 ">Username:</label>
+               <input type="text"  className="items-center h-14 w-full max-w-md border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
                onChange={(e) => setUsernameInput(e.target.value)}
                value={usernameInput}
                />
             </div>
             <div className="flex flex-row">
-               <label htmlFor="" className="mb-2 p-3">Password:</label>
-               <input type="text"  className="border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
+               <label htmlFor="" className="w-32 text-lg mb-2 p-3">Password:</label>
+               <input type="text"  className="items-center h-14 w-full max-w-md border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
                onChange={(e) => setPassword(e.target.value)}
                value={password}
                />
             </div>
             <div className="flex flex-row">
-               <label htmlFor="" className="mb-2 p-3">Password:</label>
-               <input type="text"  className="border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
+               <label htmlFor="" className="w-32 text-lg mb-2 p-3">Confirm Password:</label>
+               <input type="text"  className="items-center h-14 w-full max-w-md border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
                onChange={(e) => setConfirmPass(e.target.value)}
                value={confirmPass}
                />
             </div>
+            {/* add back button */}
          </div>
       </form>
    )
