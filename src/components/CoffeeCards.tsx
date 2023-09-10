16a501260cb50277d1106273f27bd83c052a34d8
Jensen Omega <omega90ej@gmail.com>
2023-09-09 23:49:38 -0700

    trying to work on card images for coffee Card

---
+++ src/components/CoffeeCards.tsx
@@ -0,0 +1,7 @@
+
+
+export const CoffeeCards = () => {
+   return(
+      <div>this is coffee cards component</div>
+   )
+}
--- src/components/Lobby.tsx
+++ src/components/Lobby.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../providers/AuthProvider";
 import { UserInformation } from "../types/types";
 
@@ -10,18 +11,51 @@ type LobbyTypes = {
 export const Lobby = () => {
   const { logoutUser, isRegister, user } = useAuth() as LobbyTypes;
 
+  const navigate = useNavigate();
+
+//   const handleLogoutButton = () => {
+//    navigate('/')
+//    logoutUser();
+//   };
+
   return (
     <>
-      <div>this is lobby comp</div>
-      {isRegister ? (
-        <div>
-          <h3>username: {user.username}</h3>
-          <h3>password: {user.password}</h3>
+      <div className="flex flex-col">
+        {isRegister ? (
+          <div className="w-40 text-lg mb-2 p-3">
+            <h3 >Hello! {user.username}</h3>
+            <h4>There are coffees to try!</h4>
+          </div>
+        ) : (
+          <div className="text-center">Not Logged In</div>
+        )}
+        <div className="mainBody">
+         <div>
+            <div>
+               {/* https://coffeeaffection.com/wp-content/uploads/2021/05/Spanish-latte-milk-and-espresso.jpg */}
+               {/* https://perfectdailygrind.com/wp-content/uploads/2016/11/latte-art-@harshlight-1024x683.jpg */}
+               <img src="" alt="" 
+               className="w-50 h-40"
+               />
+            </div>
+            <div>
+               <h3>Title Of Coffee</h3>
+               <p>description of coffee</p>
+            </div>
+         </div>
+         <div>
+            <div>
+               <img src="" alt="" />
+            </div>
+            <div>
+               <h3>Title Of Coffee2</h3>
+               <p>description of coffee2</p>
+            </div>
+         </div>
         </div>
-      ) : (
-        <div>Not Logged In</div>
-      )}
-      <button onClick={() => logoutUser()}>Logout</button>
+
+        <button onClick={() => logoutUser()}>Logout</button>
+      </div>
     </>
   );
 };
