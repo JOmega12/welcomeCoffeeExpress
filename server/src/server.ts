93e0d641c19bd9a0e8aeec74dca7ad5010d49f52
Jensen <omega90ej@gmail.com>
2024-01-31 22:57:36 -0800

    added more info on sever.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,27 +1,35 @@
-
-
-
-
-// import express from 'express';
-
-
-// const app = express();
-
-
 import { PrismaClient } from '@prisma/client'
+import express from 'express';
 
 const prisma = new PrismaClient()
+const app = express();
+app.use(express.json());
 
-async function main() {
-  // ... you will write your Prisma Client queries here
-}
+// INDEX
+// This allows to view ALL the coffee
+app.get("/coffee", async (req, res) => {
+  const coffee = await prisma.coffee.findMany({});
+  res.send(coffee)
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
+})
+
+// SHOW ENDPOINT
+// specific number of the id
+app.get("coffee/:id", async (req,res) => {
+  const id = +req.params.id;
+
+  const coffee = await prisma.coffee.findUnique({
+    where: {
+      id, 
+    }
   })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+
+  // shows if the item is there using the id
+  if(!coffee) {
+    return res.status(204).send("No Content")
+  }
+  res.send(coffee);
+})
+
+// this launches the port 3000
+app.listen(3000);
